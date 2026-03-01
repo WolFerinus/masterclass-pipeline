39af500cefa4038c52d01886d80dbf5c8ee1eeae
WolFerinus <eduardomartins0706@gmail.com>
2026-03-01 15:16:29 -0300

    Criando pipeline do projeto com actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://automationpratice.com.br/',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
-  },
+    projectId: "hy51yj",
+    e2e: {
+        baseUrl: 'https://automationpratice.com.br/',
+        specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'cypress/support/e2e.js',
+    },
 });
